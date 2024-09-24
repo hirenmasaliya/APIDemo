2bfd221083e59b38b076e2b60bfdfb3d2a812bd1
hirenmasaliya <112078959+hirenmasaliya@users.noreply.github.com>
2024-09-24 17:53:40 +0530

    New Files Add

--- APIDemo/Cell/TableViewCell.swift
+++ APIDemo/Cell/TableViewCell.swift
@@ -9,10 +9,6 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
-
-    @IBOutlet weak var txtPunchLine: UILabel!
-    @IBOutlet weak var txtSetup: UILabel!
-
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
